1d35f88bbf8d5ec4bef0a7b009761a2faa91c901
augustaspocius <nesuspameris@gmail.com>
2023-03-08 12:26:53 +0200

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+  import { createApp } from 'vue';
+
+
+  const card = createApp({
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js + TypeScript App'
+      }
+    }
+  })
+</script>
+
+<template>
+  <div class="text-3xl">
+    <span class="font-bold underline">
+        Hello world!
+    </span>
+    <span class="font-bold underline">
+        devfinder
+    </span>
+    <span class="font-bold underline">
+        {{ card.msg }}
+    </span>
+    <span class="text-primary">
+        Light
+        Dark
+
+        Search GitHub username...
+        Search
+
+        Joined
+
+        Repos
+        Followers
+        Following
+    </span>
+  </div>
+</template>
+
+
+<style>
+.card{
+    
+}
+</style>
