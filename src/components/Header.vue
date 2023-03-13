fa7d71eba4c5abc65982a69f6e1a7b610021d4e6
augustaspocius <nesuspameris@gmail.com>
2023-03-13 15:47:51 +0200

    WIP, add Vue files, tailwind config

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { createApp } from 'vue';
+
+</script>
+
+<template>
+  <div class="container header font-bold flex flex-row justify-between items-center">
+    <span class="text-2xl">
+      devfinder
+    </span>
+    <div class="uppercase text-sm cursor-pointer">
+        <a class="dark:text-dm_darkblue">
+          <span class="dark:">Light</span>
+          <img class="inline" src="assets/icon-sun.svg"/>
+        </a>
+      </div>
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  import { createApp } from 'vue';
+
+</script>
+
+<template>
+
+    <div class="container card text-xl flex flex-col 
+            rounded-2xl border-0 bg-dm_darkblue">
+        
+            <div class="profile flex flex-row">
+                <div class="image">
+                    <img src="" alt="profile image"/>
+                </div>
+                
+                <div class="info flex-col">
+                    <div class="name">Name</div>
+                    <div class="handle">@Handle</div>
+                    <div class="joined">Joined</div>
+                </div>
+            </div>
+            
+            <div class="description">
+                Bio
+            </div>
+            
+            <div class="stats flex flex-row">
+                Repos
+                Followers
+                Following
+            </div>
+    </div>
+
+</template>
+
+<style>
+
+</style>
